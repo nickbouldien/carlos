89b1bf91ce08ef15a8dbf647691e74b47a46b351
nick bouldien <nick.bouldien@gmail.com>
2019-10-27 12:35:21 -0500

    refactor the EnvData component

--- src/components/EnvData.js
+++ src/components/EnvData.js
@@ -1,12 +1,5 @@
 import React from "react";
 
-
-const Display = ({ info }) => {
-  return (
-    info.map(item => <li>{item}</li>)
-  )
-};
-
 class EnvData extends React.Component {
   state = {
     info: [],
@@ -14,10 +7,12 @@ class EnvData extends React.Component {
 
   async componentDidMount() {
     const data = await window.env();
-    const info = [];
-    for (const type in data) {
-      info.push(`${type}: ${data[type]}`);
-    }
+
+    const info = Object.entries(data).map(entry => ({
+      key: entry[0],
+      value: entry[1]
+    }));
+
     this.setState(prevState => ({
       ...prevState,
       info,
@@ -25,11 +20,17 @@ class EnvData extends React.Component {
   }
 
   render() {
+    const { info } = this.state;
+
     return (
       <div>
         <h3>Env data</h3>
         <ul>
-          { this.state.info && this.state.info.length && <Display info={this.state.info} /> }
+          {
+            info && info.length
+              ? info.map(item => <li key={item.key}>{item.key} = {item.value}</li>)
+              : <li>no env data to display</li>
+            }
         </ul>
       </div>
     )
